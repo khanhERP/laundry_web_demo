82a2dd0f4a7244f987c1a36e51e2989656dbd407
DESKTOP-54NCMET\PC-InfoPlus <truongpv@infoplusvn.com>
2025-11-01 08:23:49 +0700

    update

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const BASE_URL = "https://bad07204-3e0d-445f-a72e-497c63c9083a-00-3i4fcyhnilzoc.pike.replit.dev"; // 👈 đổi theo domain backend của bạn
+const BASE_URL = "https://9be1b990-a8c1-421a-a505-64253c7b3cff-00-2h4xdaesakh9p.sisko.replit.dev"; // 👈 đổi theo domain backend của bạn
 
 export async function defaultFetcher({ queryKey }) {
   const [path] = queryKey;
